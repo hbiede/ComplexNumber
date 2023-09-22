147d46acd8592566a9ef562563abc4afe99122e1
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2023-09-22 09:41:54 -0600

    Initial Commit

---
+++ ComplexNumbers/ComplexNumbers.h
@@ -0,0 +1,18 @@
+//
+//  ComplexNumbers.h
+//  ComplexNumbers
+//
+//  Created by Elijah on 9/22/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComplexNumbers.
+FOUNDATION_EXPORT double ComplexNumbersVersionNumber;
+
+//! Project version string for ComplexNumbers.
+FOUNDATION_EXPORT const unsigned char ComplexNumbersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComplexNumbers/PublicHeader.h>
+
+
