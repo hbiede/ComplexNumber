c082f9a993e4fdb4eabc1023839bac9d673d2821
HBiede <6586509+hbiede@users.noreply.github.com>
2025-03-05 01:05:57 -0700

    Update to better Swift standards

--- ComplexNumbers/ComplexNumber.swift
+++ ComplexNumbers/ComplexNumber.swift
@@ -7,36 +7,66 @@
 
 import Foundation
 
-struct ComplexNumber : Equatable,
-                       ExpressibleByIntegerLiteral,
-                       Numeric,
-                       SignedNumeric {
-    
+struct ComplexNumber {
+
     private var a, b: Double
 
-    typealias IntegerLiteralType = Int
-    init(a: Double, b: Double) {
-        self.a = a
-        self.b = b
+    init(_ a: some FloatingPoint, _ b: some FloatingPoint) {
+        self.a = Double(a)
+        self.b = Double(b)
     }
-    init(integerLiteral value: Int) {
-        a = Double(value)
-        b = 0
-    }
-    init?<T>(exactly source: T) where T : BinaryInteger {
+    init?(exactly source: BinaryInteger) {
         a = Double(source)
         b = 0
     }
 
+    func conjugate() -> ComplexNumber {
+        ComplexNumber(a: self.a, b: -1 * self.b)
+    }
+}
+
+extension ComplexNumber: CustomStringConvertible {
+    var description: String {
+        if b == .zero {
+            return "\(a)"
+        } else if a == .zero {
+            return "\(b)i"
+        } else if b > 0 {
+            return "\(a) + \(b)i"
+        } else {
+            return "\(a) - \(abs(b))i"
+        }
+    }
+}
+
+extension ComplexNumber: Equatable {
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.a == rhs.a && lhs.b == rhs.b
+    }
+}
+
+extension ComplexNumber: ExpressibleByIntegerLiteral {
+    typealias IntegerLiteralType = Int
+    init(integerLiteral value: Int) {
+        a = Double(value)
+        b = 0
+    }
+}
+
+extension ComplexNumber: ExpressibleByFloatLiteral {
+    typealias FloatLiteralType = Double
+    init(floatLiteral value: Double) {
+        a = value
+        b = 0
+    }
+}
+
+extension ComplexNumber: AdditiveArithmetic, SignedNumeric {
     typealias Magnitude = Double
     var magnitude: Double {
         get {
             sqrt(pow(a, 2) + pow(b, 2))
         }
-    }
-
-    func conjugate() -> ComplexNumber {
-        ComplexNumber(a: self.a, b: -1 * self.b)
     }
 
     mutating func negate() {
@@ -83,8 +113,10 @@ struct ComplexNumber : Equatable,
         lhs.a = (lhs.a * rhs.a + lhs.b * rhs.b) / (pow(rhs.a, 2) + pow(rhs.b, 2))
         lhs.b = (lhs.b * rhs.a - origA * rhs.b) / (pow(rhs.a, 2) + pow(rhs.b, 2))
     }
+}
 
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        return lhs.a == rhs.a && lhs.b == rhs.b
+extension FloatingPoint {
+    var i: ComplexNumber {
+        ComplexNumber(0, self)
     }
 }
